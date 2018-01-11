7a732066b5d864e093aa6fea1b2e26a967814e28
Jonas Klarstrup <jonas@klarstrup.dk>
2018-01-11 22:04:44 +0100

    remove unused branch

--- src/geometry.ts
+++ src/geometry.ts
@@ -25,9 +25,7 @@ export const polygonArea = (points: IPoint[]) => {
   const l = points.length;
   let det = 0;
 
-  if (points[0] !== points[points.length - 1]) {
-    points = points.concat(points[0]);
-  }
+  points = points.concat(points[0]);
 
   for (let i = 0; i < l; i++) {
     det += points[i].x * points[i + 1].y - points[i].y * points[i + 1].x;
