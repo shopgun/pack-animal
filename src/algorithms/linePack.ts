d914707033c01d6f68291fda7d84ef2ce564a256
klarstrup <jonas@klarstrup.dk>
2018-03-01 13:37:07 +0100

    feat(linePack): now respects gravity

--- src/algorithms/linePack.ts
+++ src/algorithms/linePack.ts
@@ -15,6 +15,17 @@ export const linePack = (
   const debug = (...args: any[]) => dbug("linePack:", ...args);
   // Write out said algorithm entry.
   debug();
+  if (vertical) {
+    return verticalLinePack(rectangleWidth, rectangleHeight, polygons);
+  }
+  return horizontalLinePack(rectangleWidth, rectangleHeight, polygons);
+};
+
+const verticalLinePack = (
+  rectangleWidth: number,
+  rectangleHeight: number,
+  polygons: IPoint[][]
+): ITransform[] => {
   const averageArea = average(polygons.map(points => polygonArea(points)));
 
   const normalizedScaleFromPoints = (points: IPoint[]) =>
@@ -22,38 +33,45 @@ export const linePack = (
   const maxWidth = Math.max(
     ...polygons.map(points => polygonWidth(points) * normalizedScaleFromPoints(points))
   );
+
+  let verticalOffset = 0;
+  return polygons.map(polygon => {
+    const normalizingScale = normalizedScaleFromPoints(polygon);
+    const horizontalCorrection = maxWidth - polygonWidth(polygon) * normalizingScale / 2;
+    const matrix = new Matrix()
+      .translate(horizontalCorrection, verticalOffset)
+      .scaleU(normalizingScale);
+    const polygonTransform = getPolygonTransform(rectangleWidth, rectangleHeight, polygon, matrix);
+    verticalOffset += polygonHeight(polygon) * normalizingScale;
+    return polygonTransform;
+  });
+};
+const horizontalLinePack = (
+  rectangleWidth: number,
+  rectangleHeight: number,
+  polygons: IPoint[][]
+): ITransform[] => {
+  const averageArea = average(polygons.map(points => polygonArea(points)));
+
+  const normalizedScaleFromPoints = (points: IPoint[]) =>
+    (averageArea / polygonArea(points) - 1) / 2 + 1;
   const maxHeight = Math.max(
     ...polygons.map(points => polygonHeight(points) * normalizedScaleFromPoints(points))
   );
 
-  // TODO: if we're at this point, we have a 1xY or Xx1 grid and are not twoferpacking
-  // We should use a separate packing algorithm specialized for these scenarios, as grids
-  // can look quite stiff or unnaturally spaced when presented with only a single row or column.
-  let horizontalOffset = 0;
-  let verticalOffset = 0;
-  const polygonTransforms = polygons.map(polygon => {
-    const normalizingScale = normalizedScaleFromPoints(polygon);
-    const horizontalCorrection = vertical
-      ? maxWidth - polygonWidth(polygon) * normalizingScale / 2
-      : 0;
-    const verticalCorrection = !vertical
-      ? maxHeight - polygonHeight(polygon) * normalizingScale / 2
-      : 0;
+  const alignBottom = true;
 
-    const polygonTransform = getPolygonTransform(
-      rectangleWidth,
-      rectangleHeight,
-      polygon,
-      new Matrix()
-        .translate(horizontalOffset + horizontalCorrection, verticalOffset + verticalCorrection)
-        .scaleU(normalizingScale)
-    );
-    if (vertical) {
-      verticalOffset += polygonHeight(polygon) * normalizingScale;
-    } else {
-      horizontalOffset += polygonWidth(polygon) * normalizingScale;
-    }
+  let horizontalOffset = 0;
+  return polygons.map(polygon => {
+    const normalizingScale = normalizedScaleFromPoints(polygon);
+    const verticalCorrection = alignBottom
+      ? maxHeight - polygonHeight(polygon) * normalizingScale
+      : maxHeight - polygonHeight(polygon) * normalizingScale / 2;
+    const matrix = new Matrix()
+      .translate(horizontalOffset, verticalCorrection)
+      .scaleU(normalizingScale);
+    const polygonTransform = getPolygonTransform(rectangleWidth, rectangleHeight, polygon, matrix);
+    horizontalOffset += polygonWidth(polygon) * normalizingScale;
     return polygonTransform;
   });
-  return polygonTransforms;
 };
